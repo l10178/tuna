3ff8a2297d3736dd6aa80f55d01529ec8fbb83ed
l10178 <l10178@163.com>
2025-03-28 08:48:39 +0800

    lint

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -13,7 +13,6 @@ import IconButton from '@mui/material/IconButton';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 import { Routes, Route, useNavigate, Navigate } from 'react-router-dom';
-import { useTheme } from '@mui/material/styles';
 
 import ApplicationShake from './components/ApplicationShake';
 import ApplicationsPage from './components/ApplicationsPage';
