148bd067c0ff5ea996880f67fbdb44aed2af61be
l10178 <l10178@163.com>
2025-05-07 00:06:25 +0800

    add backend service api

---
+++ backend/src/test/java/com/nxest/tuna/backend/TunaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nxest.tuna.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TunaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
